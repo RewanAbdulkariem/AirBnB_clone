a307b3625747c0340280e936dff3138ad4a55471
RewanAbdulkariem <rewan.khaled21@gmail.com>
2024-04-09 14:20:35 +0200

    task 4

--- models/base_model.py
+++ models/base_model.py
@@ -16,11 +16,32 @@ class BaseModel:
         created_at (datetime): Date and time when the instance was created.
         updated_at (datetime): Date and time when it was last updated.
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialize a new BaseModel instance."""
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            if 'id' in kwargs:
+                self.id = kwargs['id']
+            else:
+                self.id = str(uuid4())
+
+            if 'created_at' in kwargs:
+                self.created_at = kwargs['created_at']
+            else:
+                self.created_at = datetime.now()
+
+            if 'updated_at' in kwargs:
+                self.updated_at = kwargs['updated_at']
+            else:
+                self.updated_at = datetime.now()
+
+            for key, value in kwargs.items():
+                if key not in ['id', 'created_at', 'updated_at', '__class__']:
+                    setattr(self, key, value)
+
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def save(self):
         """Update the `updated_at` attribute with the current datetime."""
